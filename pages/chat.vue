1c32a8e24a3e054ae7fa82efa5c916f878fa93a4
andr999 <andreykuchay2@gmail.com>
2020-07-29 10:30:34 +0300

    First commit

---
+++ pages/chat.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="c-wrap">
+      <div class="c-hat" ref="block">
+        <Message 
+       v-for="m in messages" :key="m.text"
+       :name="m.name"
+       :text="m.text"
+       :owner="m.id === user.id"
+      />
+      </div>
+      <div class="c-form">
+          <ChatForm />
+      </div>
+    </div>
+</template>
+
+<script>
+import {mapState}  from 'vuex'
+import Message from '@/components/Message'
+import ChatForm from '@/components/ChatForm'
+export default {
+    computed: mapState(["user","messages"]),
+    middleware: ["chat"],
+    head() {
+        return {
+            title: `Комната ${this.user.room}`
+        }
+    },
+    components: {Message, ChatForm},
+    watch: {
+        messages(){
+            setTimeout(() => {
+            this.$refs.block.scrollTop =  this.$refs.block.scrollHeight
+            })
+            
+        }
+    }
+}
+</script>
+<style scoped>
+ .c-wrap {
+     height: 100%;
+     position: relative;
+     overflow: hidden;  
+ }
+ .c-form {
+   position: absolute;
+   bottom: 0;
+   left: 0;
+   right: 0;
+   padding: 1rem;
+   height: 80px;
+   background: #212121;
+ }
+ .c-chat {
+     position: absolute;
+     top: 0;
+     right: 0;
+     left: 0;
+     bottom: 80px;
+     padding: 1rem;
+     overflow-y: auto;
+ }
+</style>
